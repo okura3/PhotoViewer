cff90106c80e9d92ee872167f04f75685415d300
Takeshi OKURA <okura3@gmail.com>
2011-10-09 12:30:29 +0900

    Dancer::Template::Xslate に config.yml の設定値を取り込む

--- lib/Dancer/Template/Xslate.pm
+++ lib/Dancer/Template/Xslate.pm
@@ -11,71 +11,64 @@ use warnings;
 use Carp;
 use Moo;
 use Dancer::Moo::Types;
+use Dancer::FileUtils qw'path';
 use Text::Xslate;
-use File::Spec;
 
 with 'Dancer::Core::Role::Template';
 
-sub debug_dump {
-  use Data::Dumper;
-  my @caller = caller(1);
-  my $debug = Data::Dumper->Dump([ \@caller, \@_ ] );
-  open my $debugf, ">>", "/tmp/photoviewer.txt" or die;
-  print $debugf $debug, "\n";
-  close $debugf;
-}
-
 has engine => (
-    is => 'rw',
-    isa => sub { ObjectOf('Text::Xslate', @_) },
+  is  => 'rw',
+  isa => sub { ObjectOf( 'Text::Xslate', @_ ) },
 );
 
-sub default_tmpl_ext { "tt" }
+sub default_tmpl_ext {".tx"}
 
 sub init {
-    my $self = shift;
+  my $self     = shift;
+  my $charset  = $self->charset;
+  my @encoding = length($charset) ? ( ENCODING => $charset ) : ();
+  my %args     = ( %{ $self->config }, );
+  my $views    = $self->{views} || '.';
+  my $path     = [$views];
+  $self->engine( Text::Xslate->new( %args, path => $path, ) );
+}
 
-    my $charset = $self->charset;
-    my @encoding = length($charset) ? ( ENCODING => $charset ) : ();
+sub _template_name {
+  my ( $self, $view ) = @_;
+  my $suffix = $self->config->{suffix} // default_tmpl_ext;
+  $view .= $suffix if $view !~ /\Q$suffix\E$/;
+  return $view;
+}
 
-    my %args = (
-        %{$self->config},
-    );
-    my $views = $self->{views} || '.';
-    my $path = [ $views, "$views/layouts" ];
+sub view {
+  my ( $self, $view ) = @_;
+  $view = $self->_template_name($view);
+  return $view;
+}
 
-    $self->engine( Text::Xslate->new(
-      %args,
-      path => $path,
-      cache => 1,
-      cache_dir => '/home/okura/github/PhotoViewer/xslate_cache',
-      syntax => 'TTerse',
-      suffix => 'tt',
-      )
-    );
+sub render_layout {
+  my ( $self, $layout, $tokens, $content ) = @_;
+  my $layout_name = $self->_template_name($layout);
+  my $layout_path = path( 'layouts', $layout_name );
+
+  # FIXME: not sure if I can "just call render"
+  $self->render( $layout_path, { %$tokens, content => $content } );
 }
 
 sub render {
-    my ($self, $template, $tokens) = @_;
-    my $path = $self->engine->{path};
-    my $views = File::Spec->rel2abs( $self->{views} );
-    unless ( grep {$_ eq $views} @$path ) {
-        my $error = qq/Couldn't change include_path to "$views"/;
-        croak $error;
-    }
-
-   (undef,undef,my $_template) = File::Spec->splitpath( $template );
-
-    my $content = eval {
-        $self->engine->render($_template, $tokens)
-    };
-
-    if (my $err = $@) {
-        my $error = qq/Couldn't render template "$err"/;
-        croak $error;
-    }
-
-    return $content;
+  my ( $self, $template, $tokens ) = @_;
+  my $path  = $self->engine->{path};
+  my $views = File::Spec->rel2abs( $self->{views} );
+  unless ( grep { $_ eq $views } @$path ) {
+    my $error = qq/Couldn't change include_path to "$views"/;
+    croak $error;
+  }
+  my $content = eval { $self->engine->render( $template, $tokens ) };
+  if ( my $err = $@ ) {
+    my $error = qq/Couldn't render template "$err"/;
+    croak $error;
+  }
+  return $content;
 }
 
 1;
